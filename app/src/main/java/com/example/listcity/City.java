229e56ac9afad108156c21aea9a8cd98573af30f
wangtaocheng <wangtaocheng@wangtaochengs-MacBook-Pro.local>
2026-02-12 18:14:12 -0700

    a

--- app/src/main/java/com/example/listcity/City.java
+++ app/src/main/java/com/example/listcity/City.java
@@ -36,12 +36,22 @@ public class City implements Comparable<City> {
     /**
      * Compare cities based on city name
      * Used for sorting cities alphabetically
+     *
+     * * @param other the other city to compare with
+     *  * @return comparison result based on city name
      */
     @Override
     public int compareTo(City other) {
         return this.city.compareTo(other.getCityName());
     }
 
+    /**
+     * Checks whether two cities are equal.
+     * Two cities are equal if both city name and province match.
+     *
+     * @param o object to compare
+     * @return true if equal, false otherwise
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -52,6 +62,12 @@ public class City implements Comparable<City> {
         return this.city.equals(other.city) && this.province.equals(other.province);
     }
 
+    /**
+     * Generates hash code for the city.
+     * Required when equals() is overridden.
+     *
+     * @return hash code value
+     */
     @Override
     public int hashCode() {
         int result = city.hashCode();
